f166bd0c14337ef22dbe99cbd9fde31a589e7990
Ubuntu <ubuntu@ip-172-31-47-222.us-east-2.compute.internal>
2021-09-09 05:34:56 +0000

    Update logs with round=3

--- exp.sh
+++ exp.sh
@@ -3,10 +3,11 @@ start=$(cat /proc/net/dev | grep "eth0")
 in_bytes=$(echo $start | awk -v OFS=, '/eth0:/ { print $2 }')
 out_bytes=$(echo $start | awk -v OFS=, '/eth0:/ { print $10 }')
 
+sudo  tc qdisc add dev eth0 root netem delay 100ms
 sudo tc qdisc change dev eth0 root netem delay 100ms 10ms 25%
 for i in {0..3}; do
 	#round cts
-	time(trickle -s -u 1024 -d 1024 ./target/release/client 3 1 2>&1) | tee -a client$i.log &
+	time(trickle -s -u 1024 -d 1024 ./target/release/client 2 1 2>&1) | tee -a client$i.log &
 done
 wait
 
@@ -15,3 +16,5 @@ in_bytes_end=$(echo $end| awk -v OFS=, '/eth0:/ { print $2 }')
 out_bytes_end=$(echo $end | awk -v OFS=, '/eth0:/ { print $10 }')
 echo "recv bytes " $((in_bytes_end - in_bytes)) >> total.log
 echo "sent bytes " $((out_bytes_end - out_bytes)) >> total.log
+
+sudo tc qdisc del dev eth0 root netem
